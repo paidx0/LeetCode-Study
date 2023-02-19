2acd58b7b742177aa7f77b54a6fcfa6dd7c2ecd9
paidx0 <3227055498@qq.com>
2023-02-20 02:16:25 +0800

    第二天

--- editor/cn/[919]完全二叉树插入器.go
+++ editor/cn/[919]完全二叉树插入器.go
@@ -1,26 +1,26 @@
-//完全二叉树 是每一层（除最后一层外）都是完全填充（即，节点数达到最大）的，并且所有的节点都尽可能地集中在左侧。 
+//完全二叉树 是每一层（除最后一层外）都是完全填充（即，节点数达到最大）的，并且所有的节点都尽可能地集中在左侧。
 //
-// 设计一种算法，将一个新节点插入到一个完整的二叉树中，并在插入后保持其完整。 
+// 设计一种算法，将一个新节点插入到一个完整的二叉树中，并在插入后保持其完整。
 //
-// 实现 CBTInserter 类: 
+// 实现 CBTInserter 类:
 //
-// 
-// CBTInserter(TreeNode root) 使用头节点为 root 的给定树初始化该数据结构； 
+//
+// CBTInserter(TreeNode root) 使用头节点为 root 的给定树初始化该数据结构；
 // CBTInserter.insert(int v) 向树中插入一个值为 Node.val == val的新节点 TreeNode。使树保持完全二叉树的状态
-//，并返回插入节点 TreeNode 的父节点的值； 
-// CBTInserter.get_root() 将返回树的头节点。 
-// 
+//，并返回插入节点 TreeNode 的父节点的值；
+// CBTInserter.get_root() 将返回树的头节点。
 //
-// 
 //
-// 
-// 
 //
-// 示例 1： 
 //
-// 
 //
-// 
+//
+//
+// 示例 1：
+//
+//
+//
+//
 //输入
 //["CBTInserter", "insert", "insert", "get_root"]
 //[[[1, 2]], [3], [4], []]
@@ -31,22 +31,21 @@
 //CBTInserter cBTInserter = new CBTInserter([1, 2]);
 //cBTInserter.insert(3);  // 返回 1
 //cBTInserter.insert(4);  // 返回 2
-//cBTInserter.get_root(); // 返回 [1, 2, 3, 4] 
+//cBTInserter.get_root(); // 返回 [1, 2, 3, 4]
 //
-// 
 //
-// 提示： 
 //
-// 
-// 树中节点数量范围为 [1, 1000] 
-// 0 <= Node.val <= 5000 
-// root 是完全二叉树 
-// 0 <= val <= 5000 
-// 每个测试用例最多调用 insert 和 get_root 操作 10⁴ 次 
-// 
+// 提示：
+//
+//
+// 树中节点数量范围为 [1, 1000]
+// 0 <= Node.val <= 5000
+// root 是完全二叉树
+// 0 <= val <= 5000
+// 每个测试用例最多调用 insert 和 get_root 操作 10⁴ 次
+//
 //
 // Related Topics 树 广度优先搜索 设计 二叉树 👍 148 👎 0
-
 
 //leetcode submit region begin(Prohibit modification and deletion)
 /**
@@ -58,24 +57,46 @@
  * }
  */
 type CBTInserter struct {
-
+	root      *TreeNode
+	candidate []*TreeNode
 }
 
-
+// 层序遍历，左右都满的加入queue，有缺的加到candidate，后面拿出来插入值就行
 func Constructor(root *TreeNode) CBTInserter {
-
+	queue := []*TreeNode{root}
+	candidate := []*TreeNode{}
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+		}
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+		}
+		if node.Left == nil || node.Right == nil {
+			candidate = append(candidate, node)
+		}
+	}
+	return CBTInserter{root, candidate}
 }
 
-
-func (this *CBTInserter) Insert(val int) int {
-
+func (c *CBTInserter) Insert(val int) int {
+	child := &TreeNode{Val: val}
+	node := c.candidate[0]
+	if node.Left == nil {
+		node.Left = child
+	} else {
+		node.Right = child
+		c.candidate = c.candidate[1:]
+	}
+	c.candidate = append(c.candidate, child)
+	return node.Val
 }
 
-
-func (this *CBTInserter) Get_root() *TreeNode {
-
+func (c *CBTInserter) Get_root() *TreeNode {
+	return c.root
 }
-
 
 /**
  * Your CBTInserter object will be instantiated and called as such:
