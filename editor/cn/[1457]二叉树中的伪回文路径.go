da3494a22410f4ec65cbfdc56c573e866ca3141a
paidx0 <3227055498@qq.com>
2023-02-25 02:40:48 +0800

    第6天

--- editor/cn/[1457]二叉树中的伪回文路径.go
+++ editor/cn/[1457]二叉树中的伪回文路径.go
@@ -1,52 +1,51 @@
-//给你一棵二叉树，每个节点的值为 1 到 9 。我们称二叉树中的一条路径是 「伪回文」的，当它满足：路径经过的所有节点值的排列中，存在一个回文序列。 
+// 给你一棵二叉树，每个节点的值为 1 到 9 。我们称二叉树中的一条路径是 「伪回文」的，当它满足：路径经过的所有节点值的排列中，存在一个回文序列。
 //
-// 请你返回从根到叶子节点的所有路径中 伪回文 路径的数目。 
+// 请你返回从根到叶子节点的所有路径中 伪回文 路径的数目。
 //
-// 
 //
-// 示例 1： 
 //
-// 
+// 示例 1：
 //
-// 
-//输入：root = [2,3,1,3,1,null,1]
-//输出：2 
-//解释：上图为给定的二叉树。总共有 3 条从根到叶子的路径：红色路径 [2,3,3] ，绿色路径 [2,1,1] 和路径 [2,3,1] 。
+//
+//
+//
+// 输入：root = [2,3,1,3,1,null,1]
+// 输出：2
+// 解释：上图为给定的二叉树。总共有 3 条从根到叶子的路径：红色路径 [2,3,3] ，绿色路径 [2,1,1] 和路径 [2,3,1] 。
 //     在这些路径中，只有红色和绿色的路径是伪回文路径，因为红色路径 [2,3,3] 存在回文排列 [3,2,3] ，绿色路径 [2,1,1] 存在回文排
-//列 [1,2,1] 。
-// 
+// 列 [1,2,1] 。
 //
-// 示例 2： 
 //
-// 
+// 示例 2：
 //
-// 
-//输入：root = [2,1,1,1,3,null,null,null,null,null,1]
-//输出：1 
-//解释：上图为给定二叉树。总共有 3 条从根到叶子的路径：绿色路径 [2,1,1] ，路径 [2,1,3,1] 和路径 [2,1] 。
+//
+//
+//
+// 输入：root = [2,1,1,1,3,null,null,null,null,null,1]
+// 输出：1
+// 解释：上图为给定二叉树。总共有 3 条从根到叶子的路径：绿色路径 [2,1,1] ，路径 [2,1,3,1] 和路径 [2,1] 。
 //     这些路径中只有绿色路径是伪回文路径，因为 [2,1,1] 存在回文排列 [1,2,1] 。
-// 
 //
-// 示例 3： 
 //
-// 
-//输入：root = [9]
-//输出：1
-// 
+// 示例 3：
 //
-// 
 //
-// 提示： 
+// 输入：root = [9]
+// 输出：1
 //
-// 
-// 给定二叉树的节点数目在范围 [1, 10⁵] 内 
-// 1 <= Node.val <= 9 
-// 
+//
+//
+//
+// 提示：
+//
+//
+// 给定二叉树的节点数目在范围 [1, 10⁵] 内
+// 1 <= Node.val <= 9
+//
 //
 // Related Topics 位运算 树 深度优先搜索 广度优先搜索 二叉树 👍 60 👎 0
 
-
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -55,7 +54,29 @@
  *     Right *TreeNode
  * }
  */
-func pseudoPalindromicPaths (root *TreeNode) int {
-
+// 位运算，要么成对异或后能消除等于0，要么就是只剩下一位为1，减1做&验证
+func pseudoPalindromicPaths(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	var res int
+	dfs(root, &res, 0)
+	return res
 }
-//leetcode submit region end(Prohibit modification and deletion)
+func dfs(root *TreeNode, res *int, temp int) {
+	n := temp ^ (1 << root.Val)
+	if root.Left == nil && root.Right == nil {
+		if n == 0 || (n&(n-1)) == 0 {
+			*res += 1
+		}
+		return
+	}
+	if root.Left != nil {
+		dfs(root.Left, res, n)
+	}
+	if root.Right != nil {
+		dfs(root.Right, res, n)
+	}
+}
+
+// leetcode submit region end(Prohibit modification and deletion)
--- editor/cn/[669]修剪二叉搜索树.go
+++ editor/cn/[669]修剪二叉搜索树.go
@@ -1,40 +1,39 @@
-//给你二叉搜索树的根节点 root ，同时给定最小边界low 和最大边界 high。通过修剪二叉搜索树，使得所有节点的值在[low, high]中。修剪树 不
-//应该 改变保留在树中的元素的相对结构 (即，如果没有被移除，原有的父代子代关系都应当保留)。 可以证明，存在 唯一的答案 。 
+// 给你二叉搜索树的根节点 root ，同时给定最小边界low 和最大边界 high。通过修剪二叉搜索树，使得所有节点的值在[low, high]中。修剪树 不
+// 应该 改变保留在树中的元素的相对结构 (即，如果没有被移除，原有的父代子代关系都应当保留)。 可以证明，存在 唯一的答案 。
 //
-// 所以结果应当返回修剪好的二叉搜索树的新的根节点。注意，根节点可能会根据给定的边界发生改变。 
+// 所以结果应当返回修剪好的二叉搜索树的新的根节点。注意，根节点可能会根据给定的边界发生改变。
 //
-// 
 //
-// 示例 1： 
-// 
-// 
-//输入：root = [1,0,2], low = 1, high = 2
-//输出：[1,null,2]
-// 
 //
-// 示例 2： 
-// 
-// 
-//输入：root = [3,0,4,null,2,null,null,1], low = 1, high = 3
-//输出：[3,2,null,1]
-// 
+// 示例 1：
 //
-// 
 //
-// 提示： 
+// 输入：root = [1,0,2], low = 1, high = 2
+// 输出：[1,null,2]
 //
-// 
-// 树中节点数在范围 [1, 10⁴] 内 
-// 0 <= Node.val <= 10⁴ 
-// 树中每个节点的值都是 唯一 的 
-// 题目数据保证输入是一棵有效的二叉搜索树 
-// 0 <= low <= high <= 10⁴ 
-// 
+//
+// 示例 2：
+//
+//
+// 输入：root = [3,0,4,null,2,null,null,1], low = 1, high = 3
+// 输出：[3,2,null,1]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 树中节点数在范围 [1, 10⁴] 内
+// 0 <= Node.val <= 10⁴
+// 树中每个节点的值都是 唯一 的
+// 题目数据保证输入是一棵有效的二叉搜索树
+// 0 <= low <= high <= 10⁴
+//
 //
 // Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 760 👎 0
 
-
-//leetcode submit region begin(Prohibit modification and deletion)
+// leetcode submit region begin(Prohibit modification and deletion)
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -43,7 +42,21 @@
  *     Right *TreeNode
  * }
  */
-func trimBST(root *TreeNode, low int, high int) *TreeNode {
 
+// 遍历所有节点，大于high的右边剪枝，小于low的左边剪枝
+func trimBST(root *TreeNode, low int, high int) *TreeNode {
+	if root == nil {
+		return root
+	}
+	if root.Val > high {
+		return trimBST(root.Left, low, high)
+	}
+	if root.Val < low {
+		return trimBST(root.Right, low, high)
+	}
+	root.Left = trimBST(root.Left, low, high)
+	root.Right = trimBST(root.Right, low, high)
+	return root
 }
-//leetcode submit region end(Prohibit modification and deletion)
+
+// leetcode submit region end(Prohibit modification and deletion)
