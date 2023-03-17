3e99b0702b85942c147d9d2b58c7318b26506b6f
paidx0 <3227055498@qq.com>
2023-03-17 22:20:27 +0800

    2023.3.17

---
+++ editor/cn/[1054]距离相等的条形码.go
@@ -0,0 +1,36 @@
+//在一个仓库里，有一排条形码，其中第 i 个条形码为 barcodes[i]。 
+//
+// 请你重新排列这些条形码，使其中任意两个相邻的条形码不能相等。 你可以返回任何满足该要求的答案，此题保证存在答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：barcodes = [1,1,1,2,2,2]
+//输出：[2,1,2,1,2,1]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：barcodes = [1,1,1,1,2,2,3,3]
+//输出：[1,3,1,3,2,1,2,1] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= barcodes.length <= 10000 
+// 1 <= barcodes[i] <= 10000 
+// 
+//
+// Related Topics 贪心 数组 哈希表 计数 排序 堆（优先队列） 👍 98 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func rearrangeBarcodes(barcodes []int) []int {
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ editor/cn/[767]重构字符串.go
@@ -0,0 +1,37 @@
+//给定一个字符串 s ，检查是否能重新排布其中的字母，使得两相邻的字符不同。 
+//
+// 返回 s 的任意可能的重新排列。若不可行，返回空字符串 "" 。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: s = "aab"
+//输出: "aba"
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: s = "aaab"
+//输出: ""
+// 
+//
+// 
+//
+// 提示: 
+//
+// 
+// 1 <= s.length <= 500 
+// s 只包含小写字母 
+// 
+//
+// Related Topics 贪心 哈希表 字符串 计数 排序 堆（优先队列） 👍 466 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func reorganizeString(s string) string {
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
